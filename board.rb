6a8002480ef5ba48bb37f1a8d808b0edef0e9608
Aaron <aaroncho10@gmail.com>
2022-02-26 22:47:34 -0500

    added [pos] and [pos] = methods

--- board.rb
+++ board.rb
@@ -4,6 +4,16 @@ class Board
         @grid = Array.new(3){Array.new(3, "_")}
     end
 
+    def [](pos)
+        row, col = pos
+        @grid[row][col]
+    end
+
+    def []= (pos, mark)
+        row, col = pos 
+        @grid[row][col] = mark 
+    end
+
     def print
         @grid.each do |row|
             puts row.join(" ")
